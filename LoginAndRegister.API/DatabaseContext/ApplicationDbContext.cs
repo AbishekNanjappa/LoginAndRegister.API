1de9f05f7a3bd947b7f647b0ff178a008b375b78
AbishekNanjappa <abishek.nanjappa@outlook.com>
2026-03-01 13:21:18 +0530

    Add project files.

---
+++ LoginAndRegister.API/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LoginAndRegister.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAndRegister.API.DatabaseContext
+{
+    public class ApplicationDbContext: Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext<SystemUser, 
+        IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ LoginAndRegister.API/Models/SystemUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoginAndRegister.API.Models
+{
+    public class SystemUser: IdentityUser
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
